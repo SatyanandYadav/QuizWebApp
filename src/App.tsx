63a4b1e57fa4dda9dd9bab14348425f24085fde4
SatyanandYadav <satyanandyadav1517@gmail.com>
2024-09-08 10:58:33 +0530

    Add context api to store quiz data

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Home, Question, Result } from "./pages";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import QuizContextProvider from "./assets/context/QuizContext";
 
 function App() {
   const queryClient = new QueryClient({
@@ -14,14 +15,16 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/question" element={<Question />} />
-          <Route path="/result" element={<Result />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </BrowserRouter>
+      <QuizContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/question" element={<Question />} />
+            <Route path="/result" element={<Result />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </BrowserRouter>
+      </QuizContextProvider>
     </QueryClientProvider>
   );
 }
---
+++ src/assets/context/QuizContext.tsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+import { IQuizContext, IReportResult } from "../../interface/context";
+
+export const QuizContext = createContext({} as IQuizContext);
+
+const QuizContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [quizId, setQuizId] = useState<string | number>("");
+  const [reportData, setReportData] = useState<IReportResult[]>([]);
+  return (
+    <QuizContext.Provider
+      value={{ quizId, setQuizId, reportData, setReportData }}
+    >
+      {children}
+    </QuizContext.Provider>
+  );
+};
+
+export default QuizContextProvider;
